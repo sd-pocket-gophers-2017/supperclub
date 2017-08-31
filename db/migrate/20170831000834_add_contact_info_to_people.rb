f186fcacd225271e3ed4fac05b33551bf2f3db23
Alexis DeRuyter & Emmet Mahdavi & Mark Kim <pair+alexistderuyter.emmetreza.trumpocalypse@devbootcamp.com>
2017-08-30 17:51:24 -0700

    add user stories to read me, switch phone data type to string

--- db/migrate/20170831000834_add_contact_info_to_people.rb
+++ db/migrate/20170831000834_add_contact_info_to_people.rb
@@ -2,6 +2,6 @@ class AddContactInfoToPeople < ActiveRecord::Migration[5.1]
   def change
     add_column :people, :last_name, :string, null: false
     add_column :people, :first_name, :string, null: false
-    add_column :people, :phone, :integer, null: false
+    add_column :people, :phone, :string, null: false
   end
 end
