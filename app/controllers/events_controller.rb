67de12a0416b70f047d666380c789378cde32ff6
Mark Kim <mark.jn.kim@gmail.com>
2017-08-31 19:39:42 -0700

    add filter for event story - admin priveleges

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -57,7 +57,7 @@ class EventsController < ApplicationController
     end
 
     def require_login
-      unless logged_in?
+      unless admin_signed_in?
         flash[:error] = "You must be logged in to access this section"
         redirect_to new_admin_session_path # halts request cycle
       end
